f33e8da0fd46d58ef6b84524e50401e6a78192a3
ivannano <iacebedoherrera@gmail.com>
2022-03-26 17:27:37 +0100

    Added dependencies to pom.xml Finished class Application.java

--- PracticaGrupalSSDD_1/src/main/java/es/codeurjc/PracticaGrupalSSDD_1/Application.java
+++ PracticaGrupalSSDD_1/src/main/java/es/codeurjc/PracticaGrupalSSDD_1/Application.java
@@ -1,9 +1,12 @@
 package es.codeurjc.PracticaGrupalSSDD_1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-    	System.out.println("Hola Mundo");
-    	
+    	SpringApplication.run(Application.class, args);
     }
 }
