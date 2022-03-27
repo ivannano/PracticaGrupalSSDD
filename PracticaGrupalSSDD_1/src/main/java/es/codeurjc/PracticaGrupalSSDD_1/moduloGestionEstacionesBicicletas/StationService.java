b8ecb5e4e76adca07a8d5650673a6ca071e0199d
david <david@DESKTOP-0N2AHHD>
2022-03-27 18:25:37 +0200

    Prueba 3 del push del proyecto --> carpte static dentro de /resources con el style.css --> templates introducidos --> clase DataBaseIni para inicializar la app con datos --> Modulo Gestion Estacion de Bicicletas introducido

---
+++ PracticaGrupalSSDD_1/src/main/java/es/codeurjc/PracticaGrupalSSDD_1/moduloGestionEstacionesBicicletas/StationService.java
@@ -0,0 +1,37 @@
+package es.codeurjc.PracticaGrupalSSDD_1.moduloGestionEstacionesBicicletas;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StationService {
+	
+	private StationRepository repository;
+	
+	public StationService(StationRepository repo) {
+		this.repository = repo;
+	}
+	
+	public Station save(Station station) {
+		Station newStation = repository.save(station);
+		return newStation;
+	}
+	
+	public void delete(Long Id) {
+		repository.deleteById(Id);
+	}
+	
+	
+	public List<Station> findAll() {
+		return repository.findAll();
+	}
+	
+	public Optional<Station> findOne(long id) {
+		return repository.findById(id);
+	}
+	
+	public boolean exist(long id) {
+		return repository.existsById(id);
+	}
+}
