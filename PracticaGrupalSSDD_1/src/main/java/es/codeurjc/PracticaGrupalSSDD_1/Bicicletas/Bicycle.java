3904b29d90c11c8808deb2b26221ddaab6a4fdc3
valor <valor@host.docker.internal>
2022-04-06 20:15:19 +0200

    Avance en Bicicletas

--- PracticaGrupalSSDD_1/src/main/java/es/codeurjc/PracticaGrupalSSDD_1/Bicicletas/Bicycle.java
+++ PracticaGrupalSSDD_1/src/main/java/es/codeurjc/PracticaGrupalSSDD_1/Bicicletas/Bicycle.java
@@ -1,5 +1,56 @@
 package es.codeurjc.PracticaGrupalSSDD_1.Bicicletas;
 
+import java.time.LocalDate;
+
 public class Bicycle {
 
+	private String n_serie;
+	private String modelo;
+	private LocalDate f_alta;
+	private Estado estado;
+	
+	private enum Estado {SIN_BASE, EN_BASE, RESERVADA, BAJA}
+	
+	public Bicycle(String ns, String m) {
+		n_serie = ns;
+		modelo = m;
+		f_alta = LocalDate.now();
+		estado = Estado.SIN_BASE;
+	}
+	
+	public String getNSerie() {
+		return this.n_serie;
+	}
+	public void setEstado(Estado e){
+		estado = e;
+	}
+	
+	@Override
+	public String toString() {
+		String s = "Bicicleta [Numero de serie: " + this.n_serie + "\nModelo: " + this.modelo + "\nFecha de alta: " + this.f_alta + "\nEstado: ";
+		if (estado == Estado.SIN_BASE) {
+			s = s + "sin base. Esta dada de alta pero no tiene estacion asignada.";
+		}
+		else if (estado == Estado.EN_BASE) {
+			s = s + "en base. La bicileta está asociada a una estacion.";
+		}
+		else if (estado == Estado.RESERVADA) {
+			s = s + "reservada. Un usuario esta haciendo uso de la bicicleta.";
+		}
+		else {
+			s = s + "baja. No esta operativa";
+		}
+		return s;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o)return true;
+		if(o == null)return false;
+		if(getClass() != o.getClass())return false;
+		
+		Bicycle b = (Bicycle) o;
+		
+		return (this.getNSerie().equals(b.getNSerie()));
+	}
 }
